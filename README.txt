448c0024e7259f37cc690b3e95b372539795ca6a
ntym4ek <mega_comp@mail.ru>
2025-07-10 10:21:33 +0300

    начальный коммит базовой темы как подмодуля

---
+++ README.txt
@@ -0,0 +1,6 @@
+
+ABOUT PROJECT
+-----------
+
+Базовая тема на основе flexbox для корпоративных проектов, разрабатываемых на Drupal 11.
+Основа сгенерирована со starterkit_theme [Starterkit documentation](https://www.drupal.org/docs/core-modules-and-themes/core-themes/starterkit-theme).
---
+++ theme-settings.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * @file
+ * Functions to support Olivero theme settings.
+ */
+
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Implements hook_form_FORM_ID_alter() for system_theme_settings.
+ */
+function base9_form_system_theme_settings_alter(&$form, FormStateInterface $form_state): void
+{
+  $form['other'] = array(
+    '#type' => 'fieldset',
+    '#title' => 'Дополнительные настройки',
+    '#weight' => 5,
+    '#collapsible' => TRUE,
+    '#collapsed' => FALSE,
+  );
+
+  $form['other']['nav-mobile-position'] = array(
+    '#type' => 'select',
+    '#title' => 'Позиция мобильного меню',
+    '#default_value' => theme_get_setting('nav-mobile-position') ?? 'left',
+    '#options' => [
+      'left' => 'Слева',
+      'right' => 'Справа',
+    ],
+  );
+  $form['other']['nav-mobile-hide-width'] = array(
+    '#type' => 'textfield',
+    '#title' => 'Breakpoint меню мобильной версии',
+    '#description' => 'Ширина экрана, начиная с которой мобильное меню заменяется на десктопное',
+    '#default_value' => theme_get_setting('nav-mobile-hide-width') ?? '1024',
+    '#options' => [
+      'left' => 'Слева',
+      'right' => 'Справа',
+    ],
+  );
+}
