63720de5250cf7568996c77c3285e6b766dfd252
ntym4ek <mega_comp@mail.ru>
2025-07-21 16:45:07 +0300

    screen-width переименован в screen-wide, js библиотека swiperThumbs в дополнение к модулю Swiper formatter, правки

---
+++ js/swiper-thumbs.js
@@ -0,0 +1,40 @@
+/**
+ * @file
+ * Добавить модулю swiper_formatter возможность вывода пагинатора в виде thumbs
+ */
+
+((Drupal, settings, once) => {
+  /**
+   * @type {Drupal~behavior}
+   *
+   * @prop {Drupal~behaviorAttach} attach
+   * Инициализировать свайперы с классом swiper-thumbs
+   * и задать их в свойстве thumbs для родительских свайперов с id,
+   * указанном в атрибуте data-parent-swiper-id
+   */
+  Drupal.behaviors.swiperThumbs = {
+    attach(context) {
+      once('swiper-thumbs-once', '.swiper-thumbs', context).forEach(
+        (element) => {
+          let parentId = element.dataset.parentSwiperId;
+
+          if (parentId) {
+            const swiper = typeof Swiper !== 'undefined' ? Swiper : (window.SwiperFormatter ?? null);
+            let sw = new swiper(element, {
+              direction: 'vertical',
+              spaceBetween: 20,
+              slidesPerView: 'auto',
+              watchSlidesProgress: true,
+            });
+
+            let parentSwiper = drupalSettings.swiper_formatter.swipers[parentId];
+            if (parentSwiper) {
+              parentSwiper.thumbs = {swiper: sw};
+            }
+          }
+        }
+      );
+    },
+  };
+
+})(Drupal, drupalSettings, once);
