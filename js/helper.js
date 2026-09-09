a71fc65147c7622ec3b70ebf7cc2d491245b63f5
ntym4ek <mega_comp@mail.ru>
2026-09-09 16:03:58 +0300

    правки

--- js/helper.js
+++ js/helper.js
@@ -5,8 +5,11 @@
   'use strict';
 
   // Универсальная функция проверки переменных на наличие значения
+  // В том числе можно проверять jQuery объекты,
+  // которые, если селектор не найден, возвращают непустой объект с length:0
   Drupal.isEmpty = function (value) {
     if (value == null) return true;
+    if (value.jquery) { return value.length === 0; }
     if (Array.isArray(value) || typeof value === 'string') return value.length === 0;
     if (value instanceof Map || value instanceof Set) return value.size === 0;
     if (typeof value === 'object') return Object.keys(value).length === 0;
